1bdae21974ef798795d6c65d4f03af63d1a7263b
Eduard Carreras <ecarreras@gisce.net>
2015-06-10 17:23:22 +0200

    Add OT101 and initial support to OT103

--- src/empowering.js
+++ src/empowering.js
@@ -1,4 +1,4 @@
-/* global Mustache */
+/* global Handlebars */
 var Empowering = {};
 
 (function() {
@@ -75,11 +75,70 @@ var Empowering = {};
 
         });
 
-        ot101.infoTemplate = Mustache.render(
-            'Has consumit un {{diffAverageConsumption}}'
+        ot101.infoTemplate = Handlebars.compile(
+            'Has consumit un <span class="empowering ot101 info">' +
+            '{{abs diffAverageConsumption}}% ' +
+            '{{#positive diffAverageConsumption}}més{{else}}menys' +
+            '{{/positive}}</span> ' +
+            'que els teus veïns/es i un <span class="empowering ot101 info">' +
+            '{{abs diffAverageEffConsumption}}% ' +
+            '{{#positive diffAverageEffConsumption}}més{{else}}menys' +
+            '{{/positive}}</span> que els teus veïns/es eficients'
         );
+
+        if (attrs.info) {
+            $('#' + attrs.info).html(ot101.infoTemplate(data));
+        }
+
+        ot101.customersTemplate = Handlebars.compile(
+            'Els teus veïns/es són <strong>{{numberCustomers}}</strong> ' +
+                'llars unifamiliars amb la mateixa potència contractada'
+        );
+
+        if (attrs.infoCustomers) {
+            $('#' + attrs.infoCustomers).html(ot101.customersTemplate(data));
+        }
 
         return ot101;
     };
 
+    Empowering.Graphics.OT103 = function(attrs) {
+        var ot103 = {};
+
+        var data = JSON.parse(attrs.data);
+
+        var averageEffConsumption = [];
+        var consumption = [];
+        var averageConsumption = [];
+
+        data.forEach(function(x) {
+            averageEffConsumption.push(x.averageEffConsumption);
+            consumption.push(x.consumption);
+            averageConsumption.push(x.averageConsumption);
+        });
+
+        ot103.plot = $.jqplot(attrs.container, [
+            averageConsumption, consumption, averageEffConsumption
+        ], {
+        });
+
+        return ot103;
+    };
+
 }());
+
+Handlebars.registerHelper({
+    positive: function(value, options) {
+        'use strict';
+        if (value >= 0) {
+            return options.fn(this);
+        }
+        else {
+            return options.inverse(this);
+        }
+    },
+    abs: function(value) {
+        'use strict';
+        return Math.abs(value);
+    }
+});
